bf36ba7899379cd1fe863032ef4edb6d43163ec0
devJS <iai6203@gmail.com>
2023-02-08 14:08:42 +0900

    메인 페이지에 3D 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 // @seo
 import { NextSeo } from 'next-seo'
-// @framer
-import { motion } from 'framer-motion'
-// @mui
-import { useTheme } from '@mui/material'
-import Badge from '@mui/material/Badge'
-import Stack from '@mui/material/Stack'
-import Typography from '@mui/material/Typography'
+// @three
+import { Canvas } from '@react-three/fiber'
+import Scene from '@/components/Models/Scene'
 //
 import MainLayout from '@/layouts/MainLayout'
 //
 import type { NextPageWithLayout } from '@/pages/_app'
 
 const Home: NextPageWithLayout = () => {
-  const theme = useTheme()
-
   return (
     <>
       <NextSeo title="HOME" />
-      <Stack
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-        sx={{ height: '100%', transform: 'translateY(-80px)' }}
-      >
-        <Badge
-          component={motion.span}
-          initial={{ y: 50, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 0.5, duration: 1 }}
-          badgeContent="Beta"
-          color="warning"
-        >
-          <Typography
-            variant="h1"
-            sx={{
-              color: theme.colors.primary.main,
-              [theme.breakpoints.up('sm')]: {
-                fontSize: '5rem',
-              },
-              [theme.breakpoints.up('lg')]: {
-                fontSize: '8rem',
-              },
-            }}
-          >
-            DEBUG
-          </Typography>
-        </Badge>
-      </Stack>
+      <Suspense fallback={null}>
+        <Canvas shadows flat linear>
+          <Scene />
+        </Canvas>
+      </Suspense>
     </>
   )
 }
